68924d726e73c1d0cf159d5e5057d9a9ad8e583a
Ibrokhim Shokirov <i.shokirov@paysera.com>
2018-12-11 11:14:44 +0300

    Logo added to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <img src="{{asset('img/logo.png')}}"/>
                 </div>
 
                 <div class="links">
