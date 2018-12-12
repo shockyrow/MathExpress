13f06c9f36feb529e2b6e05654db13ec406dd023
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2018-12-12 17:51:52 +0300

    Welcome page modified.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,9 @@
               type="text/css"
         >
 
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css" integrity="sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h" crossorigin="anonymous">
+
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}"
               rel="stylesheet"
