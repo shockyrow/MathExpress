2dbe12deb4dc8581334cbec893b4adc64f0ceeb5
Muso <musooff@gmail.com>
2018-12-12 23:56:13 +0900

    Some updates on docs

--- resources/views/docs/list.blade.php
+++ resources/views/docs/list.blade.php
@@ -4,24 +4,18 @@
     <div class="container">
         <div class="row justify-content-center">
             @for($i = 0; $i < 5; $i++)
-                <div class="col-12 col-md-6 col-lg-12">
-                    <div class="p-0 card shadow mb-3">
-                        <div class="card-body">
-                            Taylor Series
-                        </div>
-                        <div class="card-footer">
-                            <div>
-                                <p>
-                                    <strong>Definition:</strong>
-                                    A Taylor series is a series expansion of a function about a point. A one-dimensional Taylor series is an expansion of a real function f(x) about a point x=a is given by
-                                </p>
-                            </div>
-                            <img src="{{ asset('img/math-example-3.png') }}"
-                            />
-                            <p>
-                                A Taylor series is a series expansion of a function about a point. A one-dimensional Taylor series is an expansion of a real function f(x) about a point x=a is given by
-                            </p>
-                        </div>
+                <div class="col-3">
+                    <div class="p-0 mb-3">
+                        <img src="{{ asset('img/book-thumb-1.jpg') }}"
+                             class="card-img-top"
+                        />
+                        <button type="button"
+                                class="btn btn-primary my-3"
+                                data-toggle="modal"
+                                data-target="#download"
+                        >
+                            Download
+                        </button>
                     </div>
                 </div>
             @endfor
