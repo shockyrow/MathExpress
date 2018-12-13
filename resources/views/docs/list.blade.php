2bcd930a7196af67e5eaeeaea613cac5354c146a
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2018-12-13 10:13:47 +0300

    docs.list view's responsivity bug fixed and modified.

--- resources/views/docs/list.blade.php
+++ resources/views/docs/list.blade.php
@@ -5,18 +5,20 @@
         <div class="row justify-content-left">
             @for($i = 0; $i < 15; $i++)
                 <div class="col-12 col-md-6 col-lg-3">
-                    <div class="p-0 card shadow mb-3"
-                    >
+                    <div class="p-0 card shadow mb-3">
                         <img src="{{ asset('img/book-thumb-1.jpg') }}"
                              class="card-img-top"
                         />
-                        <button type="button"
-                                class="btn btn-primary my-2 mx-2"
-                                data-toggle="modal"
-                                data-target="#bookView"
-                        >
-                            Read
-                        </button>
+
+                        <div class="card-body">
+                            <button type="button"
+                                    class="btn btn-outline-primary btn-block shadow-sm"
+                                    data-toggle="modal"
+                                    data-target="#bookView"
+                            >
+                                Read
+                            </button>
+                        </div>
                     </div>
                 </div>
             @endfor
@@ -30,9 +32,9 @@
          aria-labelledby="bookViewLabel"
          aria-hidden="true"
     >
-        <div class="container h-100 d-flex align-items-center">
+        <div class="container">
 
-            <div class="modal-dialog mx-0 w-100"
+            <div class="modal-dialog modal-dialog-centered"
                  role="document"
                  style="max-width: initial !important;"
             >
@@ -54,29 +56,45 @@
                     <div class="modal-body p-0">
                         <div class="container justify-content-center">
                             <div class="row">
-                                <img src="{{ asset('img/book-thumb-1.jpg') }}"
-                                    class="mx-2 my-2" 
-                                />
+                                <div class="col-12 col-md-6 col-lg-4 my-2">
+                                    <img src="{{ asset('img/book-thumb-1.jpg') }}"
+                                         class="w-100 rounded shadow-sm"
+                                    />
+                                </div>
                                 <div class="col my-2">
                                     <h3>
                                         Force of Nature
                                     </h3>
-                                    <h5>
-                                        <small>by</small> Muso Khon
-                                    </h5>
+                                    <h4>
+                                        <small>by</small>
+                                        Muso Khon
+                                    </h4>
                                     <hr>
-                                    <p>
-                                        From Pete Souza, the #1 New York Times bestselling author of Obama: An Intimate Portrait, comes a potent commentary on the Presidency--and our country.
-
-                                        As Chief Official White House Photographer, Pete Souza spent more time alongside President Barack Obama than almost anyone else. His years photographing the President gave him an intimate behind-the-scenes view of the unique gravity of the Office of the Presidency--and the tremendous responsibility that comes with it.
+                                    <p class="text-justify">
+                                        From Pete Souza, the #1 New York Times bestselling author of Obama: An Intimate
+                                        Portrait, comes a potent commentary on the Presidency--and our country.
+                                        As Chief Official White House Photographer, Pete Souza spent more time alongside
+                                        President Barack Obama than almost anyone else. His years photographing the
+                                        President gave him an intimate behind-the-scenes view of the unique gravity of
+                                        the Office of the Presidency--and the tremendous responsibility that comes with
+                                        it.
                                     </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-primary">Download</button>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button"
+                                class="btn btn-primary"
+                        >
+                            Download
+                        </button>
+                        <button type="button"
+                                class="btn btn-secondary"
+                                data-dismiss="modal"
+                        >
+                            Close
+                        </button>
                     </div>
                 </div>
             </div>
