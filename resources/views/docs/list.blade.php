180dd0985666eeb28ba1cb4a445c6e8a5787c5bf
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2018-12-17 09:04:29 +0300

    docs/list view modified.

--- resources/views/docs/list.blade.php
+++ resources/views/docs/list.blade.php
@@ -2,45 +2,16 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-left">
+        <div class="row justify-content-center">
             @for($i = 0; $i < 15; $i++)
-                <div class="col-12 col-md-6">
-                    <div class="p-0 card shadow mb-3">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-12 col-lg-5">
-                                    <img src="{{ asset('img/book-thumb-1.jpg') }}"
-                                         class="rounded shadow-sm w-100"
-                                    />
-                                    <button type="button"
-                                            class="btn btn-outline-primary btn-block shadow-sm my-3"
-                                            data-toggle="modal"
-                                            data-target="#bookView"
-                                    >
-                                        Read
-                                    </button>
-                                </div>
-                                <div class="col">
-                                    <h3>
-                                        Force of Nature
-                                    </h3>
-                                    <h4>
-                                        <small>by</small>
-                                        Muso Khon
-                                    </h4>
-                                    <hr>
-                                    <p class="text-justify">
-                                        From Pete Souza, the #1 New York Times bestselling author of Obama: An Intimate
-                                        Portrait, comes a potent commentary on the Presidency--and our country.
-                                        As Chief Official White House Photographer, Pete Souza spent more time alongside
-                                        President Barack Obama than almost anyone else. His years photographing the
-                                        President gave him an intimate behind-the-scenes view of the unique gravity of
-                                        the Office of the Presidency--and the tremendous responsibility that comes with
-                                        it.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="btn bg-transparent p-0 mb-3"
+                         data-toggle="modal"
+                         data-target="#bookView"
+                    >
+                        <img src="{{ asset('img/book-thumb-1.jpg') }}"
+                             class="rounded shadow w-100"
+                        />
                     </div>
                 </div>
             @endfor
