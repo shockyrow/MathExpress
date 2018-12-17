979004aeb88f533ccad798196364cbf0abadd02c
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2018-12-17 17:04:59 +0300

    db.sql corrected for Mac systems.

--- db.sql
+++ db.sql
@@ -1,4 +1,5 @@
 create schema mx_db;
-create user mx@'localhost' identified by 'secret';
+create user mx@'localhost' identified with mysql_native_password by 'secret';
 grant all on mx_db.* to mx@'localhost';
 flush privileges;
+
