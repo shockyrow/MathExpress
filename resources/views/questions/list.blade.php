db51074164f3fd2f956cd46740b7b1ca7d97d7e9
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2018-12-17 10:32:41 +0300

    Questions list modified and issue #3 fixed.

--- resources/views/questions/list.blade.php
+++ resources/views/questions/list.blade.php
@@ -21,64 +21,46 @@
                     <a href="{{ route('questions.show', 1) }}"
                        class="btn p-0 card shadow mb-3 text-justify"
                     >
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col p-2 d-flex justify-content-center">
-                                    @if($i % 3 == 0)
-                                        <button type="button"
-                                                class="btn mr-2 btn-success align-self-center h-100"
-                                        >
-                                            <h2>5</h2>
-                                            <small>чавобхо</small>
-                                        </button>
-                                    @elseif ($i % 3 == 1)
-                                        <button type="button"
-                                                class="btn mr-2 btn-primary-outline align-self-center h-100"
-                                        >
-                                            <h2>0</h2>
-                                            <small>чавобхо</small>
-                                        </button>
-                                    @else
-                                        <button type="button"
-                                                class="btn mr-2 btn-outline-success align-self-center h-100"
-                                        >
-                                            <h2>2</h2>
-                                            <small>чавобхо</small>
-                                        </button>
-                                    @endif
+                        <div class="card-body d-flex align-items-center p-3">
+                            <h5 class="flex-grow-1 pr-2"
+                                style="white-space: normal"
+                            >
+                                Масохати секунчаро читавр меёбед?
+                                <div class="small mt-2">
+                                    <span class="badge badge-secondary mr-1 p-1">геометрия</span>
+                                    <span class="badge badge-secondary mr-1 p-1">граф</span>
+                                    <span class="badge badge-secondary mr-1 p-1">сентроид</span>
+                                </div>
+                            </h5>
+                            <div>
+                                @if($i % 3 == 0)
                                     <button type="button"
-                                            class="btn btn-primary-outline align-self-center h-100"
+                                            class="btn btn-sm mr-2 btn-success align-self-center h-100"
                                     >
-                                        <h2>15</h2>
-                                        <small>дидан</small>
+                                        <h2 class="mb-0">5</h2>
+                                        <small>чавобхо</small>
                                     </button>
-                                </div>
-                                <div class="col-12 col-md-10 p-3">
-                                    <h3 class="mb-0"
-                                        style="white-space: normal;"
+                                @elseif ($i % 3 == 1)
+                                    <button type="button"
+                                            class="btn btn-sm mr-2 btn-primary-outline align-self-center h-100"
                                     >
-                                        Масохати секунчаро читавр меёбед?
-                                    </h3>
-                                    <h5>
-                                        <small>
-                                            <strong>
-                                                Али Валиев
-                                            </strong>
-                                            пурсидааст
-                                        </small>
-                                    </h5>
-                                    <div class="d-flex">
-                                        <span class="badge badge-secondary mr-1">
-                                            геометрия
-                                        </span>
-                                        <span class="badge badge-secondary mr-1">
-                                            граф
-                                        </span>
-                                        <span class="badge badge-secondary mr-1">
-                                            сентроид
-                                        </span>
-                                    </div>
-                                </div>
+                                        <h2 class="mb-0">0</h2>
+                                        <small>чавобхо</small>
+                                    </button>
+                                @else
+                                    <button type="button"
+                                            class="btn btn-sm mr-2 btn-outline-success align-self-center h-100"
+                                    >
+                                        <h2 class="mb-0">2</h2>
+                                        <small>чавобхо</small>
+                                    </button>
+                                @endif
+                                <button type="button"
+                                        class="btn btn-sm btn-primary-outline align-self-center h-100"
+                                >
+                                    <h2 class="mb-0">15</h2>
+                                    <small>дидан</small>
+                                </button>
                             </div>
                         </div>
                     </a>
