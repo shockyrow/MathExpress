d56791ea44306b6af6e370a1288a53dba8860172
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2019-03-21 17:35:50 +0300

    Login responsibility fixed

--- code/resources/views/auth/login.blade.php
+++ code/resources/views/auth/login.blade.php
@@ -1,47 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row my-5">
-            <div class="col-3"></div>
-            <div class="col-3 text-center">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <div class="input-group d-flex align-items-center m-2">
-                        <i class="fa fa-user"></i>
-                        <input id="email" placeholder="Сурогаи электрони" type="email" style="border: 0; box-shadow: none"
-                               class="form-control" name="email"
-                               value="{{ old('email') }}" required autofocus>
-                    </div>
-                    <hr class="m-0" style="background-color: {{ $errors->has('email') ? '#e3342f' : ''}}">
-                    <div class="input-group d-flex align-items-center m-2">
-                        <i class="fa fa-lock"></i>
-                        <input id="password" placeholder="Парол" type="password" style="border: 0; box-shadow: none"
-                               class="form-control" name="password" value="{{ old('password') }}"
-                               required>
-                    </div>
-                    <hr class="m-0" style="background-color: {{ $errors->has('email') ? '#e3342f' : ''}}">
+    <div class="container my-3">
+        <div class="row d-flex flex-wrap justify-content-center flex-row-reverse">
+            <div class="p-5">
+                <img class="w-100" src="{{URL::asset('/img/login-page.png')}}">
+            </div>
+            <div class="col col-lg-4 d-flex align-items-center">
+                <div class="col text-center">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-user"></i></span>
+                                </div>
 
-                    <div class="my-4">
-                        @if (Route::has('password.request'))
-                            <a class="btn btn-link" style="color: #4e555b" href="{{ route('password.request') }}">
-                                {{ __('Паролро фаромуш кардед?') }}
-                            </a>
-                        @endif
+                                <input id="email" placeholder="Сурогаи электрони" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                            </div>
+                        </div>
+
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fa fa-lock"></i></span>
+                            </div>
+
+                            <input id="password" placeholder="Парол" type="password" class="form-control" name="password" value="{{ old('password') }}" required>
+                        </div>
+
+                        <div>
+                            @if (Route::has('password.request'))
+                                <a class="btn btn-link" style="color: #4e555b" href="{{ route('password.request') }}">
+                                    {{ __('Паролро фаромуш кардед?') }}
+                                </a>
+                            @endif
+                        </div>
+
+                        <button type="submit" class="btn btn-success mt-3 w-75" style="border-radius: 20px">
+                            {{ __('Логин') }}  <i class="fa fa-angle-right"></i>
+                        </button>
+                    </form>
+
+                    <div class="my-2">
+                        Суратхисоб надоред?
+                        <a href="{{ route('register') }}" style="color: #e3342f">
+                            Бакайдгиред
+                        </a>
                     </div>
-                    <button type="submit" class="btn btn-success my-4 w-75" style="border-radius: 20px">
-                        {{ __('Логин') }}  <i class="fa fa-angle-right"></i>
-                    </button>
-                </form>
-            </div>
-            <div class="col-3">
-                <img src="{{URL::asset('/img/login-page.png')}}">
-            </div>
-        </div>
-        <div class="row my-5">
-            <div class="col-3"></div>
-            <div class="col-3">
-                <p style="font-size: small">Суратхисоб надоред?<a class="btn btn-link" href="{{ route('register') }}" style="color: #e3342f">Бакайдгиред</a></p>
+                </div>
             </div>
         </div>
     </div>
