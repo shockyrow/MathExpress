a1762d887cb3b7413c4de26815352adee36abbff
Muso <musooff@gmail.com>
2019-03-22 03:42:29 +0900

    updated indicators with arrows

--- code/resources/views/layouts/components/header.blade.php
+++ code/resources/views/layouts/components/header.blade.php
@@ -68,15 +68,6 @@
 
         @if (isset($showCarousel) && $showCarousel)
             <div id="mainCarouselControls" class="carousel slide carousel-fade my-5" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#mainCarouselControls" data-slide-to="0" class="active"></li>
-                    <li data-target="#mainCarouselControls" data-slide-to="1"></li>
-                    <li data-target="#mainCarouselControls" data-slide-to="2"></li>
-                    <li data-target="#mainCarouselControls" data-slide-to="3"></li>
-                    <li data-target="#mainCarouselControls" data-slide-to="4"></li>
-                    <li data-target="#mainCarouselControls" data-slide-to="5"></li>
-                    <li data-target="#mainCarouselControls" data-slide-to="6"></li>
-                </ol>
                 <div class="carousel-inner">
                     <div class="carousel-item carousel-item-overlay active">
                         <img src="{{ asset('img/slider/welcome_slide_1.png') }}" class="d-block w-100" alt="...">
@@ -100,6 +91,16 @@
                         <img src="{{ asset('img/slider/welcome_slide_7.png') }}" class="d-block w-100" alt="...">
                     </div>
                 </div>
+
+                <a class="carousel-control-prev" href="#mainCarouselControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+
+                <a class="carousel-control-next" href="#mainCarouselControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
         @endif
     </div>
